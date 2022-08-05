a9f74365685efe5e73b858f5ff3689e94a47f9b3
Thibault Touzin <thibault.touzin@gmail.com>
2022-08-05 16:06:57 +0200

    paginator start

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,7 +5,10 @@ namespace App\Controller;
 use App\Repository\ActualityRepository;
 use App\Repository\ForumRepository;
 use App\Services\GameApi;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -15,11 +18,16 @@ class HomeController extends AbstractController
      * @Route("/",name="home")
      */
 
-    public function home(GameApi $gameApi, ActualityRepository $actualityRepository, ForumRepository $forumRepository){
+    public function home(GameApi $gameApi, Request $request ,ActualityRepository $actualityRepository, ForumRepository $forumRepository, PaginatorInterface $paginator){
 
 //        dd($gameApi->getGames());
         $games = $gameApi->getGamesHome();
         $news = $actualityRepository->findAll();
+        $page = $paginator->paginate(
+            $news,
+            $request->query->getInt('page',1),
+            12
+        );
         $forums = $forumRepository->findAll();
         return $this->render('home.html.twig',[
             'games'=>$games,
